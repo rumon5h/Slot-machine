88159a6b481886cb9a80edf8521b27dff85a6d46
rumon5h <mdrumonkhan48519@gmail.com>
2023-01-11 21:59:53 +0600

    get deposit and validate it

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Slot machine")
+def deposit():
+    while True:
+        amount = input("What would you like to deposit? $")
+        if amount.isdigit():
+            amount = int(amount)
+            if amount > 0:
+                break
+            else:
+                print("Amount must be greater than 0.")
+        else:
+            print("Please enter a number.")
+
+    return amount
+
+
+deposit()
